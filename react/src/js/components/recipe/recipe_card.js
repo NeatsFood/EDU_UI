2600bcd4c0c1afbd49a91e2d772518bdc1cced62
Jake Rye <jrye@mit.edu>
2019-08-28 11:16:24 -0400

    slight style cleanup

--- react/src/js/components/recipe/recipe_card.js
+++ react/src/js/components/recipe/recipe_card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Card, CardBody, CardTitle, CardFooter, CardImg} from 'reactstrap';
+import { Button, Card, CardBody, CardTitle, CardFooter, CardImg } from 'reactstrap';
 
 /**
  * RecipeCard
@@ -13,65 +13,69 @@ import {Button, Card, CardBody, CardTitle, CardFooter, CardImg} from 'reactstrap
  */
 export class RecipeCard extends React.Component {
 
-    onSelectRecipe = (e) => {
-        this.props.onSelectRecipe(e.target.value);
-    };
+  onSelectRecipe = (e) => {
+    this.props.onSelectRecipe(e.target.value);
+  };
 
-    onSaveRecipe = (e) => {
-        this.props.onSaveRecipe(e.target.value, null);
-    };
+  onSaveRecipe = (e) => {
+    this.props.onSaveRecipe(e.target.value, null);
+  };
 
-    onShareRecipe = (e) => {
-        this.props.onSaveRecipe(e.target.value, 'true');
-    };
+  onShareRecipe = (e) => {
+    this.props.onSaveRecipe(e.target.value, 'true');
+  };
 
-    onDeleteRecipe = (e) => {
-        this.props.onDeleteRecipe(e.target.value);
-    };
+  onDeleteRecipe = (e) => {
+    this.props.onDeleteRecipe(e.target.value);
+  };
 
-    onEditRecipe = (e) => {
-        this.props.onEditRecipe(e.target.value);
-    };
+  onEditRecipe = (e) => {
+    this.props.onEditRecipe(e.target.value);
+  };
 
-    render() {
-        let edit_but;
-        let delete_but;
-        let save_but;
-        let share_but;
-        if(this.props.users_recipe) {
-            edit_but = <Button size="sm" value={this.props.recipe.recipe_uuid}
-                onClick={this.onEditRecipe}>Edit</Button>;
-            delete_but = <Button size="sm" value={this.props.recipe.recipe_uuid}
-                onClick={this.onDeleteRecipe}>Delete</Button>;
-            share_but = <Button size="sm" value={this.props.recipe.recipe_uuid}
-                onClick={this.onShareRecipe}>Share</Button>;
-        } else { // for all common recipes
-            save_but = <Button size="sm" value={this.props.recipe.recipe_uuid}
-                onClick={this.onSaveRecipe}>Save</Button>;
-        }
-        return (
-            <Card>
-                <CardImg top src="https://cdn.shopify.com/s/files/1/0156/0137/products/refill_0012_basil.jpg?v=1520501227" alt=''/>
-                <CardBody>
-                    <CardTitle>
-                        {this.props.recipe.name}<br/>
-                        <small>{this.props.by_user}</small> 
-                    </CardTitle>
+  render() {
+    // Get parameters
+    const { recipe } = this.props;
+    const imageUrl = recipe["image_url"] || "https://cdn.shopify.com/s/files/1/0156/0137/products/refill_0012_basil.jpg?v=1520501227";
 
-                    <h6 className="text-muted">
-                        {this.props.recipe.recipe_json.description.brief}
-                    </h6>
-                </CardBody>
-                <CardFooter>
-                    {edit_but}{' '}
-                    <Button size="sm" value={this.props.recipe.recipe_uuid}
-                      onClick={this.onSelectRecipe}>View</Button>{' '}
-                    {delete_but}{' '}
-                    {save_but}{' '}
-                    {share_but}
-                </CardFooter>
-            </Card>
-        )
+    let edit_but;
+    let delete_but;
+    let save_but;
+    let share_but;
+    if (this.props.users_recipe) {
+      edit_but = <Button size="sm" value={this.props.recipe.recipe_uuid}
+        onClick={this.onEditRecipe}>Edit</Button>;
+      delete_but = <Button size="sm" value={this.props.recipe.recipe_uuid}
+        onClick={this.onDeleteRecipe}>Delete</Button>;
+      share_but = <Button size="sm" value={this.props.recipe.recipe_uuid}
+        onClick={this.onShareRecipe}>Share</Button>;
+    } else { // for all common recipes
+      save_but = <Button size="sm" value={this.props.recipe.recipe_uuid}
+        onClick={this.onSaveRecipe}>Save</Button>;
     }
+    return (
+      <Card>
+        <CardImg top src={imageUrl} alt='' />
+        <CardBody>
+          <CardTitle>
+            {this.props.recipe.name}<br />
+            <small>{this.props.by_user}</small>
+          </CardTitle>
+
+          <h6 className="text-muted">
+            {this.props.recipe.recipe_json.description.brief}
+          </h6>
+        </CardBody>
+        <CardFooter>
+          {edit_but}{' '}
+          <Button size="sm" value={this.props.recipe.recipe_uuid}
+            onClick={this.onSelectRecipe}>View</Button>{' '}
+          {delete_but}{' '}
+          {save_but}{' '}
+          {share_but}
+        </CardFooter>
+      </Card>
+    )
+  }
 
 }
