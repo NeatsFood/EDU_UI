d20c6e1ec44f3b8d6b003a8f13e81f34e163e4f2
Jake Rye <jrye@mit.edu>
2019-10-22 13:42:53 -0400

    added very basic landing page

--- react/src/js/components/Landing.js
+++ react/src/js/components/Landing.js
@@ -1,22 +1,56 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
 import { useAuth0 } from "../react-auth0-wrapper";
+import { Container, Card, CardHeader, CardBody, CardText, CardFooter, CardImg, CardTitle, Button, } from "reactstrap";
+import lab from '../../images/laboratory-analyst.png'
+
 
 function Landing() {
-  const { loading, isAuthenticated } = useAuth0();
+  const { loading, isAuthenticated, loginWithRedirect } = useAuth0();
 
   if (loading) {
-    return (
-      <div>Loading...</div>
-    );
+    return '';
+  }
+
+  if (isAuthenticated) {
+    return <Redirect to="dashboard" />
   }
 
   return (
-    <div>
-      Landing...{isAuthenticated ? 'Authenticated' : 'Not Authenticated'}
-      {isAuthenticated && <Redirect to="/dashboard" />}
-    </div>
+    <Container style={{ display: 'flex', justifyContent: 'center' }} >
+      <Card className="text-center" style={{ marginTop: '50px', width: '400px' }}>
+        <CardHeader style={{ fontSize: '20px' }}>
+          {/* Welcome! */}
+          Welcome to the OpenAg Education UI
+        </CardHeader>
+        <CardBody>
+          <CardTitle>
+          </CardTitle>
+          <CardImg top style={{ width: '280px', marginBottom: '10px' }} src={lab} alt="Card image cap" />
+          <CardText style={{ margin: 20 }}>
+            Connect a device, start a recipe, and view you data.
+            Create an account to get started.
+          </CardText>
+          <Button onClick={() => loginWithRedirect()}>Create Account</Button>
+        </CardBody>
+        <CardFooter className="text-muted">
+          <span style={{ flexDirection: 'row' }}>
+            Or, check out the <a style={{ display: "table-cell" }} href="https://forum.openag.media.mit.edu" rel="noopener noreferrer" target="_blank">forum</a>.
+          </span>
+        </CardFooter>
+      </Card>
+    </Container>
   )
 }
 
 export default Landing;
+
+
+{/* <div style={{ alignItems: 'center', justifyItems: 'center' }}> */ }
+{/* <Card style={{ width: 300 }}> */ }
+{/* <CardImg top width="100%" src={lab} alt="Card image cap" /> */ }
+{/* Landing...{isAuthenticated ? 'Authenticated' : 'Not Authenticated'} */ }
+{/* {isAuthenticated && <Redirect to="/dashboard" />} */ }
+{/* <img src={lab} alt="Card Icon" /> */ }
+{/* </Card> */ }
+{/* </div> */ }
